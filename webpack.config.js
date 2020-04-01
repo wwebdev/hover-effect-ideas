f0e76d822a6faff0e7ae0b5eb6ef0b447cef1997
Vincenius <vincet.will@vaitrade.de>
2020-04-01 08:33:17 +0200

    init hover effects repo

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    entry: './src/js/main.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'eslint-loader'
+            },
+            {
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }]
+    }
+}
